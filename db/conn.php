3eb748044bb91f1d2e9670ea7e37e3850bc7d6fb
Betonk26 <betonk26@gmail.com>
2022-08-07 22:05:19 +0700

    edit err code

--- db/conn.php
+++ db/conn.php
@@ -1,7 +1,7 @@
 <!-- connect to localhost -->
 <?php 
     $conn   = mysqli_connect("localhost", "root", "", "java_db");
-    if(mysqli_errno($conn)){
-        echo "tidak dapat connect ke database";
+    if(!$conn){
+        die("ga bisa konek");
     }
 ?>
